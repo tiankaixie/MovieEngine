a02f67474b3390b2672b5e8fb9d5ab4c35b7756d
Neonnsnow <tiankaixie@gmail.com>
2017-03-22 11:26:51 -0400

    pass the search result to a view
    passed the search result to a view.. next is to rate and give the recommands

--- hello.py
+++ hello.py
@@ -1,6 +1,14 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+import numpy as np
+from scipy.spatial.distance import pdist, squareform
+from scipy.io import loadmat
+from scipy.optimize import fmin_cg
+
+import plotly.plotly as py
+import plotly.graph_objs as go
+import re
 from wtforms import Form, BooleanField, StringField, PasswordField, validators
 
 class RegistrationForm(Form):
@@ -13,13 +21,42 @@ class RegistrationForm(Form):
 	confirm = PasswordField('Repeat Password')
 	accept_tos = BooleanField('I accept the TOS', [validators.DataRequired()])
 
+class SearchForm(Form):
+    name = StringField('Moviename')
+
+def loadMovie():
+    bucket = []
+    file = open('movie_ids.txt', 'r')
+    for line in file:
+        info = line.split(' ',1)
+        bucket.append(info[1][:-1])
+    # print bucket
+    return bucket
+
+def searchMovie(movieName):
+    print 'enter search:'
+    movieList = loadMovie()
+    res_idx = [i for i, movie in enumerate(movieList) if re.search(movieName, movie, re.I)]
+    print res_idx
+    movieList = np.asarray(movieList)
+    print 'going to return'
+    return movieList[res_idx]
+
+
 app = Flask(__name__)
 
 
 
-@app.route('/')
+@app.route('/',methods=['GET', 'POST'])
 def index():
-    return render_template('base.html')
+    form = SearchForm(request.form)
+    if request.method == 'POST' and form.validate():
+        print 'The form is received. '
+        print form.name.data
+        search_res = searchMovie(form.name.data)
+        print 'returned'
+        return render_template('result.html', search_res = search_res)
+    return render_template('search.html', form = form)
 
 
 @app.route('/searchById/<int:movie_id>')
